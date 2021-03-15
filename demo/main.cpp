c9bb7b50ac2f4b77a3662ddeb566c32c9c285778
ak-kuznetsov <a.kuznetsov2701@mail.ru>
2021-03-16 02:19:19 +0300

    Add main.cpp

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,5 +1,9 @@
-#include <header.hpp>
+// Copyright 2021 ak-kuznetsov <a.kuznetsov2701@mail.ru>
 
-int main() {
+#include <Hash_calculator.hpp>
 
+int main(int argc, char* argv[]) {
+  signal(SIGINT, Hash_calculator::Signal_catch);
+  Hash_calculator hash(argc, argv);
+  hash.Start(true);
 }
